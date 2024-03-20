29dc9d50df9f8475238d0b4111bec94162ae7830
KULBHUSHAN <krishnathakur0007895@gmail.com>
2024-03-20 15:50:38 +0530

    successfully fetched data from tmdb api and done browse page main container

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,37 +1,61 @@
-import { signOut } from "firebase/auth";
-import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { auth } from "../utils/firebase";
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { LOGO } from "../utils/constants";
+import { addUser, removeUser } from "../utils/userSlice";
 
 const Header = () => {
-    const navigate = useNavigate();
-    const user = useSelector((store) => store.user);
-    const handleSignOut = () => {
-        signOut(auth)
-            .then(() => {
-                navigate("/");
-            })
-            .catch((error) => {
-                navigate("/error");
-            });
-    };
-    return (
-            <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
-                <img
-                    className="w-44"
-                    src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
-                    alt="logo"
-                />
-                {user && (
-                    <div className="flex p-2">
-                        <img className="w-12 h-12" alt="usericon" src={user?.photoURL} />
-                        <button onClick={handleSignOut} className="font-bold text-white ">
-                            (Sign Out)
-                        </button>
-                    </div>
-                )}
-            </div>
-            );
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const user = useSelector((store) => store.user);
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => { })
+      .catch((error) => {
+        navigate("/error");
+      });
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const { uid, email, displayName, photoURL } = user;
+        dispatch(
+          addUser({
+            uid: uid,
+            email: email,
+            displayName: displayName,
+            photoURL: photoURL,
+          })
+        );
+        navigate("/browse");
+      } else {
+        dispatch(removeUser());
+        navigate("/");
+      }
+    });
+
+    // Unsiubscribe when component unmounts
+    return () => unsubscribe();
+  }, []);
+
+
+  return (
+    <div className=" absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
+      <h1 className=" w-screen font-bold text-red-600  bg-gradient-to-b from-black text-4xl px-8 py-2">GPT<span className="">Film</span>Finder</h1>
+
+      {user && (
+        <div className="flex p-2">
+          <img className="w-12 h-12" alt="usericon" src={user?.photoURL} />
+          <button onClick={handleSignOut} className="font-bold text-white ">
+            (Sign Out)
+          </button>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Header;
---
+++ src/utils/constants.js
@@ -0,0 +1,12 @@
+
+
+export const USER_AVATAR =
+    "https://occ-0-6247-2164.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e";
+
+export const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3OTMzYzdmNTRkZmJmMDYyMjkwMTI2Y2U0ZDY3NTZmZCIsInN1YiI6IjY1MzNhZDExOGNmY2M3MDEwYWY1NzViMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.N3k0sTQRpRdplp783VwJpVYalVG2hCHXPMJtvnTtoXs'
+    }
+};
